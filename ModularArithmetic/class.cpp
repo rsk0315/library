cf2954f8304b470cca290388f7e6de6baef9925a
rsk0315 <rsk0315@icloud.com>
2020-01-18 02:51:41 +0900

    Add modular arithmetics

---
+++ ModularArithmetic/class.cpp
@@ -0,0 +1,159 @@
+/**
+ * @brief 合同算術用クラス
+ * @author えびちゃん
+ */
+
+#include <type_traits>
+#include <utility>
+
+template <typename Tp, Tp Modulo>
+class modint {
+public:
+  using value_type = typename std::make_signed<Tp>::type;
+
+private:
+  static constexpr value_type S_mod = Modulo;
+  value_type M_value = 0;
+  value_type M_mod;  // runtime value (used if S_mod == 0)
+
+  static constexpr value_type S_inv(value_type n, value_type m) {
+    value_type x = 0;
+    value_type y = 1;
+    value_type a = n;
+    value_type b = m;
+    for (value_type u = y, v = x; a;) {
+      value_type q = b / a;
+      std::swap(x -= q*u, u);
+      std::swap(y -= q*v, v);
+      std::swap(b -= q*a, a);
+    }
+    if ((x %= m) < 0) x += m;
+    return x;
+  }
+
+  static value_type S_normalize(value_type n, value_type m) {
+    if (n >= m) {
+      n %= m;
+    } else if (n < 0) {
+      if ((n %= m) < 0) n += m;
+    }
+    return n;
+  }
+
+public:
+  modint() = default;
+  modint(modint const&) = default;
+  modint(modint&&) = default;
+  template <typename Up = Tp, typename std::enable_if<(Modulo > 0), Up>::type* = nullptr>
+  modint(value_type n):
+    M_value(S_normalize(n, Modulo)) {}
+  template <typename Up = Tp, typename std::enable_if<(Modulo == 0), Up>::type* = nullptr>
+  modint(value_type n, value_type m):
+    M_value(S_normalize(n, m)), M_mod(m) {}
+  // copying mod
+  modint(value_type n, modint const& base):
+    M_value(S_normalize(n, base.modulo())), M_mod(base.M_mod) {}
+
+  modint& operator =(modint const&) = default;
+  modint& operator =(modint&&) = default;
+  modint& operator =(value_type n) {
+    M_value = S_normalize(n, modulo());
+    return *this;
+  }
+
+  modint& operator +=(modint const& other) {
+    if ((M_value += other.M_value) >= modulo()) M_value -= modulo();
+    return *this;
+  }
+  modint& operator -=(modint const& other) {
+    if ((M_value -= other.M_value) < 0) M_value += modulo();
+    return *this;
+  }
+  modint& operator *=(modint const& other) {
+    (M_value *= other.M_value) %= modulo();
+    return *this;
+  }
+  modint& operator /=(modint const& other) {
+    (M_value *= S_inv(other.M_value, modulo())) %= modulo();
+    return *this;
+  }
+
+  modint& operator +=(value_type const& n) {
+    if ((M_value += S_normalize(n, modulo())) >= modulo()) M_value -= modulo();
+    return *this;
+  }
+  modint& operator -=(value_type const& n) {
+    if ((M_value -= S_normalize(n, modulo())) < 0) M_value += modulo();
+    return *this;
+  }
+  modint& operator *=(value_type const& n) {
+    (M_value *= S_normalize(n, modulo())) %= modulo();
+    return *this;
+  }
+  modint& operator /=(value_type const& n) {
+    (M_value *= S_inv(S_normalize(n, modulo()), modulo())) %= modulo();
+    return *this;
+  }
+
+  modint operator +(modint const& other) const { return modint(*this) += other; }
+  modint operator -(modint const& other) const { return modint(*this) -= other; }
+  modint operator *(modint const& other) const { return modint(*this) *= other; }
+  modint operator /(modint const& other) const { return modint(*this) /= other; }
+  modint operator +(value_type const& n) const { return modint(*this) += n; }
+  modint operator -(value_type const& n) const { return modint(*this) -= n; }
+  modint operator *(value_type const& n) const { return modint(*this) *= n; }
+  modint operator /(value_type const& n) const { return modint(*this) /= n; }
+
+  modint operator +() const { return *this; }
+  modint operator -() const {
+    if (M_value == 0) return *this;
+    return modint(modulo()-M_value, *this);
+  }
+
+  bool operator ==(modint const& other) const {
+    return M_value == other.M_value;
+  }
+  bool operator ==(value_type const& n) const {
+    return M_value == S_normalize(n, modulo());
+  }
+  bool operator !=(modint const& other) const { return !(*this == other); }
+  bool operator !=(value_type const& n) const { return !(*this == n); }
+
+  value_type get() const { return M_value; }
+  value_type modulo() const { return ((S_mod > 0)? S_mod: M_mod); }
+
+  static value_type generator(value_type p) {
+    // XXX
+    if (p == 998244353) return 3;
+    return -1;
+  }
+};
+
+template <typename T1, typename T2, T2 Modulo>
+modint<T2, Modulo> operator +(T1 const& lhs, modint<T2, Modulo> const& rhs) {
+  return rhs + lhs;
+}
+template <typename T1, typename T2, T2 Modulo>
+modint<T2, Modulo> operator -(T1 const& lhs, modint<T2, Modulo> const& rhs) {
+  return -(rhs - lhs);
+}
+template <typename T1, typename T2, T2 Modulo>
+modint<T2, Modulo> operator *(T1 const& lhs, modint<T2, Modulo> const& rhs) {
+  return rhs * lhs;
+}
+template <typename T1, typename T2, T2 Modulo>
+modint<T2, Modulo> operator /(T1 const& lhs, modint<T2, Modulo> const& rhs) {
+  return modint<T2, Modulo>(lhs, rhs) / rhs;
+}
+template <typename T1, typename T2, T2 Modulo>
+bool operator ==(T1 const& lhs, modint<T2, Modulo> const& rhs) {
+  return rhs == lhs;
+}
+template <typename T1, typename T2, T2 Modulo>
+bool operator !=(T1 const& lhs, modint<T2, Modulo> const& rhs) {
+  return !(lhs == rhs);
+}
+
+// constexpr intmax_t mod = 1'000'000'007;  // '
+// constexpr intmax_t mod = 998244353;
+using mi = modint<intmax_t, mod>;
---
+++ ModularArithmetic/operations.cpp
@@ -0,0 +1,63 @@
+template <typename ModInt>
+ModInt pow(ModInt const& n, intmax_t iexp) {
+  ModInt res(1, n);
+  for (ModInt dbl = n; iexp; iexp >>= 1) {
+    if (iexp & 1) res *= dbl;
+    dbl *= dbl;
+  }
+  return res;
+}
+
+template <typename ModInt>
+ModInt sqrt(ModInt const& n) {
+  if (n == 0) return n;
+
+  using value_type = typename ModInt::value_type;
+
+  intmax_t const p = modulo();
+  if (p % 4 == 3) {
+    ModInt r = pow(n, (p+1) / 4);
+    if (r * r == n) return r;
+    throw std::logic_error("quadratic nonresidue");
+  }
+
+  value_type s = ctz(p-1);
+  value_type q = (p-1) >> s;
+
+  ModInt z;
+  for (value_type z0 = 2; z0 < p; ++z0) {
+    z = ModInt(z0, n);
+    if (pow(z, (p-1) / 2) == -1) break;
+  }
+
+  value_type m = s;
+  ModInt c = pow(z, q);
+  ModInt t = pow(n, q);
+  ModInt r = pow(n, (q+1) / 2);
+
+  while (true) {
+    if (t == 0) return 0;
+    if (t == 1) return r;
+
+    value_type i = 0;
+    for (auto tt = t; tt != 1; ++i) tt *= tt;
+    if (i == m) throw std::logic_error("quadratic nonresidue");
+    auto b = c;
+    for (value_type j = 0; j < m-i-1; ++j) b *= b;
+    m = i;
+    c = b * b;
+    t *= c;
+    r *= b;
+  }
+}
+
+template <typename ModInt>
+std::vector<ModInt> sqrt_all(ModInt const& n) {
+  try {
+    auto r = sqrt(n);
+    if (r == -r) return {r};
+    return {r, -r};
+  } catch (std::logic_error const&) {
+    return {};
+  }
+}
