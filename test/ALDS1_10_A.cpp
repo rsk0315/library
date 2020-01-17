97515a1453c950653933c72092c15d0bca3fd650
rsk0315 <rsk0315@icloud.com>
2020-01-17 23:54:20 +0900

    Add a test

---
+++ test/ALDS1_10_A.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_A"
+
+#include "../utility/make/fix_point.cpp"
+
+#include <cstdio>
+#include <cstdint>
+#include <vector>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  std::vector<intmax_t> memo(n+1);
+  memo[0] = memo[1] = 1;
+  make_fix_point([&](auto f, int i) {
+      intmax_t& res = memo[i];
+      if (res > 0) return res;
+      res = f(i-1) + f(i-2);
+      return res;
+  })(n);
+  printf("%jd\n", memo[n]);
+}
