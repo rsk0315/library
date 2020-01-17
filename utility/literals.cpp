896172290d641a051a0cf4d403aa9b4ade85e32f
rsk0315 <rsk0315@icloud.com>
2020-01-17 23:16:05 +0900

    Add literals

---
+++ utility/literals.cpp
@@ -0,0 +1,12 @@
+/**
+ * @brief ユーザ定義リテラル
+ * @author えびちゃん
+ */
+
+#include <cstddef>
+#include <cstdint>
+
+constexpr intmax_t  operator ""_jd(unsigned long long n) { return n; }
+constexpr uintmax_t operator ""_ju(unsigned long long n) { return n; }
+constexpr size_t    operator ""_zu(unsigned long long n) { return n; }
+constexpr ptrdiff_t operator ""_td(unsigned long long n) { return n; }
