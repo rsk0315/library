bc434efb4ce39e893d680d177b0ae81671148d36
rsk0315 <rsk0315@icloud.com>
2020-03-02 03:46:28 +0900

    Fix impl

--- String/sa_is.cpp
+++ String/sa_is.cpp
@@ -38,115 +38,97 @@ private:
     return h;
   }
 
-  static std::vector<size_type> S_sa_is(std::vector<size_type>& c) {
+  static std::vector<size_type> S_reduce(
+      std::vector<size_type> const& c, std::vector<size_type> const& lmss,
+      std::vector<bool> const& lms
+  ) {
+    std::vector<size_type> h(lmss.size(), 0);
+    if (h.size() > 1) h[1] = 1;
+    for (size_type i = 2; i < h.size(); ++i) {
+      bool ident = (c[lmss[i]] == c[lmss[i-1]]);
+      for (size_type j = 1; ident; ++j) {
+        if (lms[lmss[i]+j] && lms[lmss[i-1]+j]) break;
+        ident &= (lms[lmss[i]+j] == lms[lmss[i-1]+j]);
+        if (ident)  // check for boundaries
+          ident &= (c[lmss[i]+j] == c[lmss[i-1]+j]);
+      }
+      h[i] = (ident? h[i-1]: h[i-1]+1);
+    }
+    std::vector<size_type> rs;
+    std::vector<size_type> map(c.size());
+    for (size_type i = 0; i < lmss.size(); ++i)
+      map[lmss[i]] = h[i];
+    for (size_type i = 0; i < c.size(); ++i)
+      if (lms[i]) rs.push_back(map[i]);
+    return rs;
+  }
+
+  static std::vector<size_type> S_sa_is(std::vector<size_type> const& c) {
     size_type n = c.size();
     enum ls_type: bool { s_type, l_type };
     std::vector<ls_type> ls(n);
     std::vector<bool> lms(n, false);
-    ls[n-1] = s_type;
-    for (size_type i = n-1; i--;) {
-      ls[i] = ((c[i] < c[i+1] || (c[i] == c[i+1] && ls[i+1] == s_type))? s_type: l_type);
-    }
 
-    std::vector<size_type> sa(n, -1);
-    std::vector<size_type> count(n);
+    // Label each suffix as S-type or L-type
+    ls[n-1] = s_type;
+    for (size_type i = n-1; i--;)
+      ls[i] = ((c[i] < c[i+1] || (c[i] == c[i+1] && ls[i+1] == s_type))? s_type: l_type);
+
+    std::vector<size_type> count(n, 0);
     for (size_type i = 0; i < n; ++i) ++count[c[i]];
 
+    std::vector<size_type> sa(n, -1);
     if (std::all_of(count.begin(), count.end(), [](auto x) { return x == 1; })) {
       // base case
       for (size_type i = 0; i < n; ++i) sa[c[i]] = i;
       return sa;
     }
 
-    std::vector<size_type> tail = count;
-    for (size_type i = 1; i < n; ++i) tail[i] += tail[i-1];
-    std::vector<size_type> head(n, 0);
-    for (size_type i = 1; i < n; ++i) head[i] = tail[i-1];
+    std::vector<size_type> tail, head;
+    auto init_offset = [&]() {
+      tail = count;
+      for (size_type i = 1; i < n; ++i) tail[i] += tail[i-1];
+      head.assign(n, 0);
+      for (size_type i = 1; i < n; ++i) head[i] = tail[i-1];
+    };
+    auto induce = [&]() {
+      for (size_type i = 0; i < n; ++i) {
+        if (sa[i] == -1_zu || sa[i] == 0 || ls[sa[i]-1] != l_type) continue;
+        sa[head[c[sa[i]-1]]++] = sa[i]-1;
+      }
+      tail = count;
+      for (size_type i = 1; i < n; ++i) tail[i] += tail[i-1];
+      for (size_type i = n; i-- > 1;) {
+        if (sa[i] == 0 || ls[sa[i]-1] != s_type) continue;
+        sa[--tail[c[sa[i]-1]]] = sa[i]-1;
+      }
+    };
 
-    // pass one
+    // Put LMS suffixes at the end of their buckets
+    init_offset();
     for (size_type i = n; i-- > 1;) {
       if (!(ls[i] == s_type && ls[i-1] == l_type)) continue;
       lms[i] = true;
       sa[--tail[c[i]]] = i;
     }
+    induce();
 
-    // pass two
-    for (size_type i = 0; i < n; ++i) {
-      if (sa[i] == -1_zu || sa[i] == 0 || ls[sa[i]-1] != l_type) continue;
-      sa[head[c[sa[i]-1]]++] = sa[i]-1;
-    }
-
-    // pass three
-    tail = count;
-    for (size_type i = 1; i < n; ++i) tail[i] += tail[i-1];
-    for (size_type i = n; i-- > 1;) {
-      if (sa[i] == 0 || ls[sa[i]-1] != s_type) continue;
-      sa[--tail[c[sa[i]-1]]] = sa[i]-1;
-    }
-
-    std::vector<size_type> lms_suffix;
-    for (size_type i = 0; i < n; ++i) {
-      if (lms[sa[i]]) lms_suffix.push_back(sa[i]);
-    }
-
-    // order-preserving hashed value
-    std::vector<size_type> h(lms_suffix.size(), 0);
-    if (h.size() > 1) h[1] = 1;
-    for (size_type i = 2; i < h.size(); ++i) {
-      bool ident = (c[lms_suffix[i]] == c[lms_suffix[i-1]]);
-      for (size_type j = 1; ident; ++j) {
-        if (lms[lms_suffix[i]+j] && lms[lms_suffix[i-1]+j]) break;
-        ident &= (lms[lms_suffix[i]+j] == lms[lms_suffix[i-1]+j]);
-        if (ident) {  // check for boundaries
-          ident &= (c[lms_suffix[i]+j] == c[lms_suffix[i-1]+j]);
-        }
-      }
-      h[i] = (ident? h[i-1]: h[i-1]+1);
-    }
-
-    std::vector<size_type> rs;  // reduced string
-    {
-      std::vector<size_type> map(n);
-      for (size_type i = 0; i < lms_suffix.size(); ++i) {
-        map[lms_suffix[i]] = h[i];
-      }
-      for (size_type i = 0; i < n; ++i) {
-        if (lms[i]) rs.push_back(map[i]);
-      }
-    }
-
-    auto rs_sa = S_sa_is(rs);
-    lms_suffix.clear();
+    std::vector<size_type> lmss;  // in sorted order
     for (size_type i = 0; i < n; ++i)
-      if (lms[i]) lms_suffix.push_back(i);
-    std::vector<size_type> sorted_lms(lms_suffix.size());
-    for (size_type i = 0; i < rs.size(); ++i)
-      sorted_lms[i] = lms_suffix[rs_sa[i]];
+      if (lms[sa[i]]) lmss.push_back(sa[i]);
 
-    tail = count;
-    for (size_type i = 1; i < n; ++i) tail[i] += tail[i-1];
-    head.assign(n, 0);
-    for (size_type i = 1; i < n; ++i) head[i] = tail[i-1];
-    sa.assign(n, -1);
+    auto rs_sa = S_sa_is(S_reduce(c, lmss, lms));
+    lmss.clear();  // reorder in appearing order
+    for (size_type i = 0; i < n; ++i)
+      if (lms[i]) lmss.push_back(i);
 
-    // pass one
-    std::reverse(sorted_lms.begin(), sorted_lms.end());
-    for (auto s: sorted_lms) sa[--tail[c[s]]] = s;
-
-    // pass two
-    for (size_type i = 0; i < n; ++i) {
-      if (sa[i] == -1_zu || sa[i] == 0 || ls[sa[i]-1] != l_type) continue;
-      sa[head[c[sa[i]-1]]++] = sa[i]-1;
+    init_offset();
+    sa.assign(n, -1_zu);
+    for (size_type i = rs_sa.size(); i--;) {
+      size_type j = lmss[rs_sa[i]];
+      sa[--tail[c[j]]] = j;
     }
-
-    // pass three
-    tail = count;
-    for (size_type i = 1; i < n; ++i) tail[i] += tail[i-1];
-    for (size_type i = n; i-- > 1;) {
-      if (sa[i] == 0 || ls[sa[i]-1] != s_type) continue;
-      sa[--tail[c[sa[i]-1]]] = sa[i]-1;
-    }
-
+    induce();
     return sa;
   }
 
