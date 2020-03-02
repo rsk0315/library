edec20419385c797bdf4e56822029f277a909a01
rsk0315 <rsk0315@icloud.com>
2020-03-03 05:31:06 +0900

    Update comment

--- test/aoj_0000.test.cpp
+++ test/aoj_0000.test.cpp
@@ -4,6 +4,8 @@
  * @title テスト
  * @author えびちゃん
  * @brief **テスト**
+ * @see https://kmyk.github.io/online-judge-verify-helper/document.ja.html
+ * @see https://github.com/kmyk/online-judge-verify-helper/blob/master/onlinejudge_verify/docs.py
  * @docs docs/test.md
  */
 
