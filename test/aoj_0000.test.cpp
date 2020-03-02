4a6370aac90c9163cb738e71df94c26a707ec751
rsk0315 <rsk0315@icloud.com>
2020-03-03 05:35:42 +0900

    Wakara-nya

--- test/aoj_0000.test.cpp
+++ test/aoj_0000.test.cpp
@@ -7,6 +7,8 @@
  * @see https://kmyk.github.io/online-judge-verify-helper/document.ja.html
  * @see https://github.com/kmyk/online-judge-verify-helper/blob/master/onlinejudge_verify/docs.py
  * @docs docs/test.md
+ * @docs test.md
+ * @docs /test.md
  */
 
 #include <cstdio>
