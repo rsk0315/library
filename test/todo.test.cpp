452b5cc61720cd2e1a2f2c831216c0dc73f4b9cb
rsk0315 <rsk0315@icloud.com>
2020-03-03 05:51:09 +0900

    Add to-do

---
+++ test/todo.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM ""
+
+/**
+ * @title To-do リスト
+ * @author えびちゃん
+ * @see https://kmyk.github.io/online-judge-verify-helper/document.ja.html
+ * @see https://github.com/kmyk/online-judge-verify-helper/blob/master/onlinejudge_verify/docs.py
+ * @docs todo.md
+ */
+
