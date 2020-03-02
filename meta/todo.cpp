0e6187e1ff518c127efd8c16e41fcaa649b5e68c
rsk0315 <rsk0315@icloud.com>
2020-03-03 05:56:22 +0900

    Add a new cat

--- meta/todo.cpp
+++ meta/todo.cpp
@@ -1,10 +1,7 @@
-#define PROBLEM ""
-
 /**
- * @title To-do リスト
- * @author えびちゃん
+ * @title to-do リスト
+ * @category (Meta)
+ * @docs todo.md
  * @see https://kmyk.github.io/online-judge-verify-helper/document.ja.html
  * @see https://github.com/kmyk/online-judge-verify-helper/blob/master/onlinejudge_verify/docs.py
- * @docs todo.md
  */
-
