f96fa4e064b859cab77f33f020c1edb2433ae2be
rsk0315 <rsk0315@icloud.com>
2020-03-15 21:09:37 +0900

    Add a monotone minima

---
+++ algorithm/monotone_minima.cpp
@@ -0,0 +1,39 @@
+/**
+ * @brief monotone minima
+ * @author えびちゃん
+ */
+
+#ifndef H_monotone_minima
+#define H_monotone_minima
+
+#include <cstddef>
+
+#ifdef CALL_FROM_TEST
+#include "utility/make/fix_point.cpp"
+#endif
+
+template <typename Fn>
+auto monotone_minima(Fn&& f, size_t h, size_t w) {
+  using value_type = decltype(f(h, w));
+  std::vector<size_t> res(h);
+
+  make_fix_point([&](auto dfs, size_t hl, size_t hu, size_t wl, size_t wu) -> void {
+      if (hl >= hu) return;
+      size_t hm = (hl+hu) >> 1;
+      value_type min = f(hm, wl);
+      res[hm] = wl;
+      for (size_t j = wl+1; j < wu; ++j) {
+        value_type cur = f(hm, j);
+        if (cur < min) {
+          min = std::move(cur);
+          res[hm] = j;
+        }
+      }
+      if (hl == hm) return;
+      dfs(hl, hm, wl, res[hm]+1);
+      dfs(hm+1, hu, res[hm], wu);
+  })(0, h, 0, w);
+  return res;
+}
+
+#endif /* !defined(H_monotone_minima) */
---
+++ test/aoj_2580.test.cpp
@@ -0,0 +1,63 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2580"
+
+#include "utility/literals.cpp"
+#include "utility/make/vector.cpp"
+#include "utility/make/fix_point.cpp"
+
+#define CALL_FROM_TEST
+#include "algorithm/monotone_minima.cpp"
+#undef CALL_FROM_TEST
+
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+int main() {
+  size_t n, m, d;
+  intmax_t x;
+  scanf("%zu %zu %zu %jd", &n, &m, &d, &x);
+
+  std::vector<intmax_t> p(n);
+  for (auto& pi: p) scanf("%jd", &pi);
+
+  std::vector<intmax_t> q(m);
+  for (auto& qi: q) {
+    intmax_t a, b;
+    scanf("%jd %jd", &a, &b);
+    qi = a - std::abs(b);
+  }
+
+  intmax_t const inf = 1e18;
+  intmax_t res = inf;
+  auto dp = make_vector({n}, inf);
+  dp[0] = 0;
+  for (size_t dd = 0; dd < d; ++dd) {
+    std::vector<intmax_t> w(n+1, 0);
+    w[0] = m;
+    for (auto const& qi: q) {
+      size_t i = std::upper_bound(p.begin(), p.end(), qi) - p.begin();
+      --w[i];
+    }
+    for (size_t i = 1; i <= n; ++i) w[i] += w[i-1];
+
+    auto tmp = make_vector({n}, inf);
+
+    auto f = [&](size_t i, size_t j) -> intmax_t {
+      if (!(j < i)) return inf;
+      return dp[j] + w[j] * (p[i]-p[j]);
+    };
+    auto prev = monotone_minima(f, n, n);
+
+    for (size_t i = 0; i < n; ++i)
+      tmp[i] = f(i, prev[i]);
+
+    dp = std::move(tmp);
+    res = std::min(res, dp[n-1]);
+    for (auto& qi: q) qi += x;
+  }
+
+  printf("%jd\n", res);
+}
