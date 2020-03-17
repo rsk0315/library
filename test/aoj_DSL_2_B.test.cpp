9d0e30bebe73b26d187d948524ceabb7e33b1733
rsk0315 <rsk0315@icloud.com>
2020-03-17 11:56:31 +0900

    Fix test

--- test/aoj_DSL_2_B.test.cpp
+++ test/aoj_DSL_2_B.test.cpp
@@ -4,52 +4,6 @@
 #include "DataStructure/basic_segment_tree.cpp"
 #undef CALL_FROM_TEST
 
-template <typename Tp>
-class sum_monoid {
-public:
-  using value_type = Tp;
-
-private:
-  value_type M_v = {};
-
-public:
-  sum_monoid() = default;  // identity
-  sum_monoid(sum_monoid const&) = default;
-  sum_monoid(sum_monoid&&) = default;
-
-  sum_monoid(value_type const& v): M_v(v) {};
-  sum_monoid(value_type&& v): M_v(std::move(v)) {};
-
-  sum_monoid& operator =(sum_monoid const&) = default;
-  sum_monoid& operator =(sum_monoid&&) = default;
-
-  sum_monoid& operator +=(sum_monoid const& that) {
-    M_v += that.M_v;
-    return *this;
-  }
-  sum_monoid& operator +=(sum_monoid&& that) {
-    M_v += std::move(that.M_v);
-    return *this;
-  }
-
-  sum_monoid operator +(sum_monoid const& that) const& {
-    return sum_monoid(*this) += that;
-  }
-  sum_monoid operator +(sum_monoid const& that) && {
-    M_v += that.M_v;
-    return *this;
-  } 
-  sum_monoid operator +(sum_monoid&& that) const& {
-    return sum_monoid(*this) += std::move(that);
-  }
-  sum_monoid operator +(sum_monoid&& that) && {
-    M_v += std::move(that.M_v);
-    return *this;
-  }
-
-  value_type const& get() const { return M_v; }
-};
-
 #include <cstdint>
 #include <cstdio>
 #include <vector>
@@ -58,7 +12,7 @@ int main() {
   size_t n, q;
   scanf("%zu %zu", &n, &q);
 
-  basic_segment_tree<sum_monoid<intmax_t>> st(n, 0);
+  basic_segment_tree<intmax_t> st(n, 0);
   for (size_t i = 0; i < q; ++i) {
     int com;
     scanf("%d", &com);
@@ -73,7 +27,7 @@ int main() {
       size_t x, y;
       scanf("%zu %zu", &x, &y);
       --x, --y;
-      printf("%jd\n", st.fold(x, y+1).get());
+      printf("%jd\n", st.fold(x, y+1));
     }
   }
 }
