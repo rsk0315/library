dad8e4b7b69368e02b799b9ccd18b90f9f972740
rsk0315 <rsk0315@icloud.com>
2020-03-17 09:47:50 +0900

    Add test

---
+++ test/yc_551.test.cpp
@@ -0,0 +1,39 @@
+#define PROBLEM "https://yukicoder.me/problems/no/551"
+
+#define CALL_FROM_TEST
+#include "ModularArithmetic/modint.cpp"
+#include "ModularArithmetic/operations.cpp"
+#undef CALL_FROM_TEST
+
+#include <cstdint>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+int main() {
+  intmax_t p;
+  int q;
+  scanf("%jd %*d %d", &p, &q);
+
+  while (q--) {
+    intmax_t a0, b0, c0;
+    scanf("%jd %jd %jd", &a0, &b0, &c0);
+
+    modint<0> a(a0, p), b(b0, p), c(c0, p);
+    auto dd = sqrt_all(b*b - 4*a*c);
+
+    std::vector<intmax_t> res;
+    for (auto d: dd)
+      res.push_back(((-b + d) / (2*a)).get());
+
+    if (res.empty()) {
+      puts("-1");
+      continue;
+    }
+
+    if (res.size() == 2 && res[0] == res[1]) res.pop_back();
+    std::sort(res.begin(), res.end());
+    for (size_t i = 0; i < res.size(); ++i)
+      printf("%jd%c", res[i], i+1<res.size()? ' ': '\n');
+  }
+}
