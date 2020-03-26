65b5f08c726c2469037e91ace3cfdba0357eff9a
rsk0315 <rsk0315@icloud.com>
2020-03-27 02:29:56 +0900

    Impl

--- DataStructure/dynamic_bitvector.cpp
+++ DataStructure/dynamic_bitvector.cpp
@@ -6,6 +6,11 @@
 #ifndef H_dynamic_bitvector
 #define H_dynamic_bitvector
 
+#ifdef CALL_FROM_TEST
+#include "utility/literals.cpp"
+#include "integer/bit.cpp"
+#endif
+
 #include <cstddef>
 #include <climits>
 #include <array>
@@ -18,6 +23,8 @@ public:
 private:
   using underlying_type = uintmax_t;
   static constexpr size_type S_ws = (CHAR_BIT * sizeof(underlying_type));
+  static constexpr size_type S_ws_2 = S_ws / 2;
+  static constexpr size_type S_ws_4 = S_ws / 4;
 
   // なんか splay tree を書く
   struct node {
@@ -31,6 +38,9 @@ private:
     size_type M_self_count = 0;
     pointer M_parent = nullptr;
     std::array<pointer, 2> M_children{{nullptr, nullptr}};
+    node() = default;
+    node(underlying_type value, size_type size):
+      M_value(value), M_self_size(size), M_self_count(popcount(value)) {}
   };
 
   using pointer = node*;  // たぶんすまーとぽいんたに変える
@@ -49,11 +59,43 @@ private:
     }
   }
 
+  static void S_insert_mini(pointer v, size_type i, bool x) {
+    // assume i < S_ws
+    printf("insert_mini(%zd, %d)\n", i, !!x);
+    underlying_type lo = v->M_value & ((1_ju << i) - 1);
+    underlying_type hi = (v->M_value ^ lo) << 1;
+    v->M_value = lo | hi;
+    ++v->M_self_size, ++v->M_size;
+    if (x) {
+      v->M_value |= 1_ju << i;
+      ++v->M_self_count, ++v->M_count;
+    }
+  }
+
+  static size_type S_select_mini(underlying_type x, size_type i) {
+    if (i == 0) return 0;
+    // TODO: speed up to O(log(ws)) time
+    for (size_t j = 0; j < S_ws; ++j) {
+      if (x >> j & 1) --i;
+      if (i == 0) return j+1;
+    }
+    return -1;  // unreachable?
+  }
+
+  static void S_inspect_dfs(pointer v) {
+    if (!v) return;
+    printf("%p [%p:%p:%p] %jx (%zu, %zu)\n",
+           v, v->M_children[0], v->M_children[1], v->M_parent,
+           v->M_value, v->M_self_size, v->M_size);
+    S_inspect_dfs(v->M_children[0]);
+    S_inspect_dfs(v->M_children[1]);
+  }
+
   void M_zig(pointer v, pointer p, size_type d) {
     v->M_parent = nullptr;
     p->M_children[d] = v->M_children[!d];
     v->M_children[!d] = p;
-    p->M_children[d]->M_parent = p;
+    if (p->M_children[d]) p->M_children[d]->M_parent = p;
     p->M_parent = v;
     S_reduce(p);
     S_reduce(v);
@@ -61,27 +103,29 @@ private:
 
   void M_zigzig(pointer v, pointer p, pointer g, size_type d) {
     v->M_parent = g->M_parent;
+    if (g->M_parent) g->M_parent->M_children[S_child_dir(g, g->M_parent)] = v;
     g->M_children[d] = p->M_children[!d];
     p->M_children[d] = v->M_children[!d];
     p->M_children[!d] = g;
     v->M_children[!d] = p;
-    g->M_children[d]->M_parent = g;
-    p->M_children[d]->M_parent = p;
-    p->M_children[!d]->M_parent = p;
-    v->M_children[!d]->M_parent = v;
+    if (g->M_children[d]) g->M_children[d]->M_parent = g;
+    if (p->M_children[d]) p->M_children[d]->M_parent = p;
+    g->M_parent = p;
+    p->M_parent = v;
     S_reduce(g);
     S_reduce(p);
     S_reduce(v);
   }
 
-  void M_zigzag(pointer v) {
+  void M_zigzag(pointer v, pointer p, pointer g, size_type d) {
     v->M_parent = g->M_parent;
+    if (g->M_parent) g->M_parent->M_children[S_child_dir(g, g->M_parent)] = v;
     p->M_children[d] = v->M_children[!d];
     g->M_children[!d] = v->M_children[d];
     v->M_children[d] = g;
     v->M_children[!d] = p;
-    g->M_children[!d]->M_parent = g;
-    p->M_children[d]->M_parent = p;
+    if (g->M_children[!d]) g->M_children[!d]->M_parent = g;
+    if (p->M_children[d]) p->M_children[d]->M_parent = p;
     g->M_parent = p->M_parent = v;
     S_reduce(g);
     S_reduce(p);
@@ -89,14 +133,14 @@ private:
   }
 
   void M_splay(pointer v) {
-    while (v->parent) {
-      pointer p = v->parent;
+    while (v->M_parent) {
+      pointer p = v->M_parent;
       size_type pd = S_child_dir(v, p);
-      if (!p->parent) {
+      if (!p->M_parent) {
         M_zig(v, p, pd);
         continue;
       }
-      pointer g = p->parent;
+      pointer g = p->M_parent;
       size_type gd = S_child_dir(p, g);
       if (pd == gd) {
         M_zigzig(v, p, g, pd);
@@ -107,10 +151,62 @@ private:
     M_root = v;
   }
 
+  size_type M_nth_bit(size_type n) {
+    pointer v = M_root;
+    while (v->M_children[0] || v->M_children[1]) {
+      size_type left = (v->M_children[0]? v->M_children[0]->M_size: 0);
+      if (n < left) {
+        v = v->M_children[0];
+      } else if (n >= left + v->M_self_size) {
+        if (!v->M_children[1]) break;  // for past-the-end one
+        n -= left + v->M_self_size;
+        v = v->M_children[1];
+      } else {
+        break;
+      }
+    }
+    if (v->M_children[0])
+      n -= v->M_children[0]->M_size;
+    M_splay(v);
+    return n;
+  }
+
   pointer M_root = nullptr;
 
 public:
-  void insert(size_type i, bool x);
+  void insert(size_type i, bool x) {
+    if (!M_root) {
+      underlying_type y = (x? 1: 0);
+      M_root = new node(y, 1);
+      return;
+    }
+
+    size_type j = M_nth_bit(i);
+    S_insert_mini(M_root, j, x);
+    if (M_root->M_self_size < S_ws) return;
+
+    // まずねー、split する。
+    pointer latter = M_root->M_children[1];
+    M_root->M_children[1] = nullptr;
+    if (latter) latter->M_parent = nullptr;
+
+    // 次にねー、word を二つに分けて、新しくできた word を追加する。
+    underlying_type lo = M_root->M_value & ((1_ju << S_ws_2) - 1);
+    underlying_type hi = M_root->M_value >> S_ws_2;
+    M_root->M_value = lo;
+    M_root->M_self_size = S_ws_2;
+    M_root->M_self_count = popcount(lo);
+    pointer inserted = new node(hi, S_ws_2);
+
+    // 最後に merge しておわりー。
+    inserted->M_children[1] = latter;
+    if (latter) latter->M_parent = inserted;
+    M_root->M_children[1] = inserted;
+    inserted->M_parent = M_root;
+    S_reduce(inserted);
+    S_reduce(M_root);
+  }
+
   void erase(size_type i);
   void set(size_type i, bool x = true);
   void reset(size_type i);
@@ -120,15 +216,59 @@ public:
   // select はセグ木上のにぶたんちっくなことをする。
 
   template <int B>
-  size_type rank(size_type i);
-  template <int B>
-  size_type select(size_type i);
+  size_type rank(size_type i) {
+    if (M_root->M_size <= i) return M_root->M_count;
+    size_type j = M_nth_bit(i);
+    size_type res = popcount(M_root->M_value & ((1_ju << j) - 1));
+    if (M_root->M_children[0]) res += M_root->M_children[0]->M_count;
+    if (B == 0) res = i - res;
+    return res;
+  }
 
-  bool operator [](size_type i) const;
+  template <int B>
+  size_type select(size_type i) {
+    pointer v = M_root;
+    size_type res = 0;
+    while (v->M_children[0] || v->M_children[1]) {
+      size_type left = (v->M_children[0]? v->M_children[0]->M_count: 0);
+      if (B == 0 && v->M_children[0]) left = v->M_children[0]->M_size - left;
+      size_type cur = v->M_count;
+      if (B == 0) cur = v->size - cur;
+      if (i < left) {
+        v = v->M_children[0];
+      } else if (i >= left + cur) {
+        if (!v->M_children[1]) break;
+        i -= left + cur;
+        res += v->M_self_size;
+        if (v->M_children[0]) res += v->M_children[0]->size;
+        v = v->M_children[1];
+      } else {
+        break;
+      }
+    }
+    if (v->M_children[0]) {
+      size_type left = v->M_children[0]->M_count;
+      if (B == 0) left = v->M_children[0]->M_size - left;
+      i -= left;
+      res += v->M_children[0]->M_size;
+    }
+    res += S_select_mini((B? res->M_value: ~res->M_value), i);
+    M_splay(v);
+    return res;
+  }
+
+  bool operator [](size_type i) {
+    size_type j = M_nth_bit(i);
+    return M_root->M_value >> j & 1;
+  }
 
   size_type size() const { return M_root->M_size; }
   size_type count() const { return M_root->M_count; }
   bool empty() const noexcept { return M_root == nullptr; }
+
+  void inspect() const {
+    S_inspect_dfs(M_root);
+  }
 };
 
 #endif  /* !defined(H_dynamic_bitvector) */
