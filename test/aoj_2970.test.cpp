493f48c3a96f64eea283e1617c094ff5e0ab1015
rsk0315 <rsk0315@icloud.com>
2020-03-31 03:49:56 +0900

    Verify CRT

---
+++ test/aoj_2970.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2970"
+
+#define CALL_FROM_TEST
+#include "ModularArithmetic/chinese_remaindering.cpp"
+#undef CALL_FROM_TEST
+
+#include <cstdint>
+#include <cstdio>
+#include <vector>
+
+int main() {
+  size_t n;
+  scanf("%zu", &n);
+
+  std::vector<size_t> p(n), q(n);
+  for (auto& pi: p) scanf("%zu", &pi), --pi;
+  for (auto& qi: q) scanf("%zu", &qi), --qi;
+
+  simultaneous_congruences sc;
+  for (size_t i = 0; i < n; ++i) {
+    int a = 0;
+    int m = 0;
+    for (size_t x = p[i], j = 0; x != i; x = q[x]) {
+      ++a;
+      if (j++ > n) return puts("-1"), 0;
+    }
+    for (size_t x = p[i], j = 0; j <= n; ++j) {
+      ++m;
+      x = q[x];
+      if (x == p[i]) break;
+      if (j == n) return puts("-1"), 0;
+    }
+    if (!sc.push(a, m)) return puts("-1"), 0;
+  }
+  printf("%jd\n", sc.get().first);
+}
