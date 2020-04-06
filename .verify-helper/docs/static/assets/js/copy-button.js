832c649670bc57a2a871dee654c45514ae301512
rsk0315 <rsk0315@icloud.com>
2020-04-07 01:41:21 +0900

    Update icons

--- .verify-helper/docs/static/assets/js/copy-button.js
+++ .verify-helper/docs/static/assets/js/copy-button.js
@@ -82,9 +82,12 @@ $(function() {
 
     let failed = '<svg width="32" height="32" class="octicon octicon-x" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><circle fill="#d73a49" cx="8" cy="8" r="8" /><path fill="white" fill-rule="evenodd" d="M9.48 8l3.75 3.75-1.48 1.48L8 9.48l-3.75 3.75-1.48-1.48L6.52 8 2.77 4.25l1.48-1.48L8 6.52l3.75-3.75 1.48 1.48L9.48 8z"></path></svg>';
 
+    let question = '<svg width="32" height="32" class="octicon octicon-q" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><circle fill="#d73a49" cx="8" cy="8" r="8" /><path fill="white" fill-rule="evenodd" d="M9 11.5c0 .28-.22.5-.5.5h-1c-.27 0-.5-.22-.5-.5v-1c0-.28.23-.5.5-.5h1c.28 0 .5.22.5.5v1zm1.56-4.89c-.06.17-.17.33-.3.47-.13.16-.14.19-.33.38-.16.17-.31.3-.52.45-.11.09-.2.19-.28.27-.08.08-.14.17-.19.27-.05.1-.08.19-.11.3-.03.11-.03.13-.03.25H7.13c0-.22 0-.31.03-.48.03-.19.08-.36.14-.52.06-.14.14-.28.25-.42.11-.13.23-.25.41-.38.27-.19.36-.3.48-.52.12-.22.2-.38.2-.59 0-.27-.06-.45-.2-.58-.13-.13-.31-.19-.58-.19-.09 0-.19.02-.3.05-.11.03-.17.09-.25.16-.08.07-.14.11-.2.2a.41.41 0 00-.09.28h-2c0-.38.13-.56.27-.83.16-.27.36-.5.61-.67.25-.17.55-.3.88-.38.33-.08.7-.13 1.09-.13.44 0 .83.05 1.17.13.34.09.63.22.88.39.23.17.41.38.55.63.13.25.19.55.19.88 0 .22 0 .42-.08.59l-.02-.01z"></path></svg>';
+
     $('img[title=":heavy_check_mark:"]').replaceWith(verified);
     $('img[title=":warning:"]').replaceWith(unverified);
     $('img[title=":x:"]').replaceWith(failed);
+    $('img[title=":question:"]').replaceWith(question);
 
     $('li svg').attr({
         width: 20,
