cd95ab17723eae23590518cc9d3ab37918a27e41
rsk0315 <rsk0315@icloud.com>
2020-04-22 02:17:04 +0900

    Rename category

--- preface/license.cpp
+++ preface/license.cpp
@@ -1,6 +1,6 @@
 /**
  * @title LICENSE
- * @category (Meta)
+ * @category !PREFACE!
  * @docs license.md
  * @see https://kimiyuki.net/blog/2020/02/14/licenses-for-kyopro-libraries/
  */
--- preface/todo.cpp
+++ preface/todo.cpp
@@ -1,6 +1,6 @@
 /**
  * @title TODO
- * @category (Meta)
+ * @category !PREFACE!
  * @docs todo.md
  * @see https://kmyk.github.io/online-judge-verify-helper/document.ja.html
  * @see https://github.com/kmyk/online-judge-verify-helper/blob/master/onlinejudge_verify/docs.py
