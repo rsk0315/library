dbf8b347df274f8674b4cbab1a4e7a3c6ede3057
rsk0315 <rsk0315@icloud.com>
2020-04-22 22:37:17 +0900

    Update for signed ones

--- integer/mul_upper.cpp
+++ integer/mul_upper.cpp
@@ -18,6 +18,7 @@ auto mul_upper(Tp u, Tp v)
   -> typename std::enable_if<std::is_integral_v<Tp>, Tp>::type
 {
   using value_type = Tp;
+  using unsigned_type = typename std::make_unsigned<Tp>::type;
   value_type hi;
   int const bits = CHAR_BIT * sizeof(value_type);
   if (false && (sizeof u) < sizeof(uintmax_t)) {
@@ -26,13 +27,13 @@ auto mul_upper(Tp u, Tp v)
     // XXX unsigned only
   } else {
     int const half_bits = bits / 2;
-    value_type const half_mask = (value_type(1) << half_bits) - 1;
-    value_type u0 = u & half_mask, v0 = v & half_mask;
-    value_type u1 = value_type(u) >> half_bits, v1 = value_type(v) >> half_bits;
-    value_type w0 = u0 * v0;
-    value_type t = u1 * v0 + (w0 >> half_bits);
-    value_type w1 = t & half_mask;
-    value_type w2 = t >> half_bits;
+    unsigned_type const half_mask = (unsigned_type(1) << half_bits) - 1;
+    unsigned_type u0 = u & half_mask, v0 = v & half_mask;
+    unsigned_type u1 = unsigned_type(u) >> half_bits, v1 = unsigned_type(v) >> half_bits;
+    unsigned_type w0 = u0 * v0;
+    unsigned_type t = u1 * v0 + (w0 >> half_bits);
+    unsigned_type w1 = t & half_mask;
+    unsigned_type w2 = t >> half_bits;
     w1 += u0 * v1;
     hi = u1 * v1 + w2 + (w1 >> half_bits);
     if (u < 0) hi -= v;
