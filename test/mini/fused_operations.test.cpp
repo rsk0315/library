da0eb36f88719cbab3ccddda8a62d5a475d0de5e
rsk0315 <rsk0315@icloud.com>
2020-04-24 02:58:22 +0900

    Add stress tests

--- test/mini/fused_operations.test.cpp
+++ test/mini/fused_operations.test.cpp
@@ -2,11 +2,82 @@
 
 #include <cassert>
 #include <climits>
+#include <limits>
+#include <random>
+#include <type_traits>
 
 #include "utility/macro/assert_eq.cpp"
 #include "integer/fused_operations.cpp"
 
 #include "test/mini/qq.cpp"
+
+  // test_mul_add();
+  // test_mul_min();
+  // test_add_mod();
+  // test_mul_mod();
+
+std::mt19937_64 rng;
+
+template <typename Tp>
+static constexpr Tp min_v = std::numeric_limits<Tp>::min();
+template <typename Tp>
+static constexpr Tp max_v = std::numeric_limits<Tp>::max();
+
+template <typename Tp>
+int test_add_mod(int rep) {
+  using large_type = typename std::conditional<std::is_signed_v<Tp>, __int128, unsigned __int128>::type;
+  std::uniform_int_distribution<Tp> u(min_v<Tp>, max_v<Tp>);
+  int neg = 0, pos = 0;
+  for (int i = 0; i < rep; ++i) {
+    Tp x = u(rng);
+    Tp y = u(rng);
+    Tp z = u(rng);
+    if (z == 0) continue;
+    Tp w = (large_type(x) + large_type(y)) % large_type(z);
+    if (w != 0 && ((w < 0) != (z < 0))) w += z;
+    std::cerr << __PRETTY_FUNCTION__ << '\n';
+    std::cerr << "fused_add_mod(" << x << ", " << y << ", " << z << "): " << w << '\n';
+    assert_eq(fused_add_mod(x, y, z), w);
+    ++((y < 0)? neg: pos);
+  }
+  assert(pos > 0 && (std::is_unsigned_v<Tp> || neg > 0));
+  return neg + pos;
+}
+
+void test_add_mod() {
+  fprintf(stderr, "%d tests passed.\n", test_add_mod<signed int>(10000));
+  fprintf(stderr, "%d tests passed.\n", test_add_mod<unsigned int>(10000));
+  fprintf(stderr, "%d tests passed.\n", test_add_mod<signed long>(10000));
+  fprintf(stderr, "%d tests passed.\n", test_add_mod<unsigned long>(10000));
+}
+
+template <typename Tp>
+int test_mul_mod(int rep) {
+  using large_type = typename std::conditional<std::is_signed_v<Tp>, __int128, unsigned __int128>::type;
+  std::uniform_int_distribution<Tp> u(min_v<Tp>, max_v<Tp>);
+  int neg = 0, pos = 0;
+  for (int i = 0; i < rep; ++i) {
+    Tp x = u(rng);
+    Tp y = u(rng);
+    Tp z = u(rng);
+    if (z == 0) continue;
+    Tp w = (large_type(x) * large_type(y)) % large_type(z);
+    if (w != 0 && ((w < 0) != (z < 0))) w += z;
+    std::cerr << __PRETTY_FUNCTION__ << '\n';
+    std::cerr << "fused_mul_mod(" << x << ", " << y << ", " << z << "): " << w << '\n';
+    assert_eq(fused_mul_mod(x, y, z), w);
+    ++((y < 0)? neg: pos);
+  }
+  assert(pos > 0 && (std::is_unsigned_v<Tp> || neg > 0));
+  return neg + pos;
+}
+
+void test_mul_mod() {
+  fprintf(stderr, "%d tests passed.\n", test_mul_mod<signed int>(10000));
+  fprintf(stderr, "%d tests passed.\n", test_mul_mod<unsigned int>(10000));
+  fprintf(stderr, "%d tests passed.\n", test_mul_mod<signed long>(10000));
+  fprintf(stderr, "%d tests passed.\n", test_mul_mod<unsigned long>(10000));
+}
 
 int main() {
   assert_eq(fused_mul_add(65536, 32768, -1), 2147483647);
@@ -64,6 +135,9 @@ int main() {
 
   assert_eq(fused_mul_mod(1000000000000000000L, 1000000000000000000L, 2305843009213693951L),
             1824060670036424877L);
+
+  test_add_mod();
+  test_mul_mod();
 
   qq();  // dummy
 }
