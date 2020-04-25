8cf26bec6c7d93f36adc8e5ebed699089cbafb1e
rsk0315 <rsk0315@icloud.com>
2020-04-26 02:32:24 +0900

    Fix interface

--- misc/interface/monoid.cpp
+++ misc/interface/monoid.cpp
@@ -1,3 +1,6 @@
+#ifndef H_monoid
+#define H_monoid
+
 /**
  * @brief モノイドクラス
  * @author えびちゃん
@@ -9,20 +12,27 @@ public:
   using value_type = Tp;
 
 private:
-  //
+  value_type M_x;
 
 public:
   monoid() = default;  // identity
 
-  monoid(value_type const&);
+  monoid(value_type const& x): M_x(x) {}
 
-  monoid& operator +=(monoid const&);
-  friend bool operator ==(monoid const& lhs, monoid const& rhs);
+  monoid& operator +=(monoid const& that) {
+    //
+    return *this;
+  }
+  friend bool operator ==(monoid const& lhs, monoid const& rhs) {
+    return lhs.M_x == rhs.M_x;
+  }
 
-  friend monoid operator +(monoid lhs, monoid const& rhs) {return lhs += rhs; }
+  friend monoid operator +(monoid lhs, monoid const& rhs) { return lhs += rhs; }
   friend bool operator !=(monoid const& lhs, monoid const& rhs) {
     return !(lhs == rhs);
   }
 
-  value_type const& get() const;
+  value_type const& get() const { return M_x; }
 };
+
+#endif  /* !defined(H_monoid) */
