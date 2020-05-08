af84e53419261dbb4c2af40d2790194e4832f802
rsk0315 <rsk0315@icloud.com>
2020-05-08 23:15:23 +0900

    Fix links

--- preface/todo.cpp
+++ preface/todo.cpp
@@ -2,6 +2,7 @@
  * @title TODO
  * @category !PREFACE!
  * @docs todo.md
- * @see https://kmyk.github.io/online-judge-verify-helper/document.ja.html
- * @see https://github.com/kmyk/online-judge-verify-helper/blob/master/onlinejudge_verify/docs.py
+ * @see https://online-judge-tools.github.io/verification-helper/document.ja.html
+ * @see https://online-judge-tools.github.io/verification-helper/installer.ja.html
+ * @see https://github.com/online-judge-tools/verification-helper/blob/master/onlinejudge_verify/docs.py
  */
