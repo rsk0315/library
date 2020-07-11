73f20f55516bd6bdb2bda120ccadb46a66992063
rsk0315 <rsk0315@icloud.com>
2020-07-11 14:49:45 +0900

    Add templates for pair

--- utility/limits.cpp
+++ utility/limits.cpp
@@ -7,8 +7,20 @@
  */
 
 #include <limits>
+#include <utility>
 
 template <typename Tp>
 class limits: public std::numeric_limits<Tp> {};
+
+template <typename T1, typename T2>
+class limits<std::pair<T1, T2>> {
+public:
+  static constexpr auto min() {
+    return std::make_pair(limits<T1>::min(), limits<T2>::min());
+  }
+  static constexpr auto max() {
+    return std::make_pair(limits<T1>::max(), limits<T2>::max());
+  }
+};
 
 #endif  /* !defined(H_limits) */
