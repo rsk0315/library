b3a6770b199e23c61ccb0eabec4e409f80eb90a9
rsk0315 <rsk0315@icloud.com>
2020-07-11 15:30:54 +0900

    Update JS

--- .verify-helper/docs/static/assets/js/copy-button.js
+++ .verify-helper/docs/static/assets/js/copy-button.js
@@ -101,7 +101,7 @@ $(function() {
     $('#verify-files').append(` <small>[passed ${numOfPassedTests} test${suffix}]</small>`);
 
     {
-        let $commitBadge = $('img[src="https://img.shields.io/github/last-commit/rsk0315/library"]');
+        let $commitBadge = $('a[href="https://github.io/rsk0315/library"]');
         // https://atcoder-badges.now.sh/
         // $commitBadge.after(' [![rsk0315](https://img.shields.io/endpoint?url=https%3A%2F%2Fatcoder-badges.now.sh%2Fapi%2Fatcoder%2Fjson%2Frsk0315)](https://atcoder.jp/users/rsk0315)');
         // ここでこれを追加してもタグになってくれないじゃん！
