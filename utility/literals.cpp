3923c56ca21ed5de99bbdd521d3893800275a45d
rsk0315 <rsk0315@icloud.com>
2020-07-11 14:53:01 +0900

    Add: [u]intN_t literals

--- utility/literals.cpp
+++ utility/literals.cpp
@@ -14,4 +14,13 @@ constexpr uintmax_t operator ""_ju(unsigned long long n) { return n; }
 constexpr size_t    operator ""_zu(unsigned long long n) { return n; }
 constexpr ptrdiff_t operator ""_td(unsigned long long n) { return n; }
 
+constexpr int8_t   operator ""_i8(unsigned long long n)  { return n; }
+constexpr int16_t  operator ""_i16(unsigned long long n) { return n; }
+constexpr int32_t  operator ""_i32(unsigned long long n) { return n; }
+constexpr int64_t  operator ""_i64(unsigned long long n) { return n; }
+constexpr uint8_t  operator ""_u8(unsigned long long n)  { return n; }
+constexpr uint16_t operator ""_u16(unsigned long long n) { return n; }
+constexpr uint32_t operator ""_u32(unsigned long long n) { return n; }
+constexpr uint64_t operator ""_u64(unsigned long long n) { return n; }
+
 #endif  /* !defined(H_int_literals) */
